73cdeb682986d6ec3214cf2c32bbd3a6f7fc89e9
Kabkav <158855852+Kabkav@users.noreply.github.com>
2025-05-31 20:43:44 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
-curl "https://github.com/lavalink-devs/Lavalink/releases/download/4.0.8/Lavalink.jar" -o Lavalink.jar
+curl -OL "https://github.com/lavalink-devs/Lavalink/releases/download/4.0.8/Lavalink.jar" Lavalink.jar
 
 java -jar Lavalink.jar
